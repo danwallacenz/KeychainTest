8a94b579ab4e56554af88b07782cecf07fa0c541
Dan Wallace <daniel.wallace.nz@gmail.com>
2018-05-09 21:43:27 +1200

    init

--- KeychainTest/ViewController.swift
+++ KeychainTest/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func setTapped(_ sender: Any) {
+        
+        let keychain = Keychain(service: "com.example.github-token")
+        DispatchQueue.global().async {
+            do {
+                // Should be the secret invalidated when passcode is removed? If not then use `.WhenUnlocked`
+                try keychain
+                    .accessibility(.whenPasscodeSetThisDeviceOnly, authenticationPolicy: .userPresence)
+                    .set("01234567-89ab-cdef-0123-456789abcdef", key: "kishikawakatsumi")
+            } catch let error {
+                // Error handling if needed...
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func getTapped(_ sender: Any) {
+        let keychain = Keychain(service: "com.example.github-token")
+        
+        DispatchQueue.global().async {
+            do {
+                let password = try keychain
+                    .authenticationPrompt("Authenticate to login to server")
+                    .get("kishikawakatsumi")
+                
+                print("password: \(String(describing: password))")
+            } catch let error {
+                // Error handling if needed...
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
